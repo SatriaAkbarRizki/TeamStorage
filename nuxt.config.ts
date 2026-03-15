c02b1f70df45c433ad756a45fb588c886b2d975e
SatriaAkbarRizki <sartiapl@gmail.com>
2026-03-15 22:27:35 +0700

    Update: using env instead hard code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@pinia/nuxt',
-    '@vueuse/nuxt'
-  ],
-  css: ['~/assets/css/main.css'],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@vueuse/nuxt"],
+  css: ["~/assets/css/main.css"],
   app: {
     head: {
-      title: 'LKPN',
+      title: "LKPN",
       link: [
-        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap' }
-      ]
-    }
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap",
+        },
+      ],
+    },
   },
   runtimeConfig: {
     public: {
-      apiBase: 'http://localhost:8080'
-    }
+      apiBase: process.env.NUXT_PUBLIC_API_BASE,
+    },
   },
-  compatibilityDate: '2025-07-15',
-  srcDir: 'app',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  srcDir: "app",
+  devtools: { enabled: true },
+});
